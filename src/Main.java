b6b0465378b53898a67789ec88f5ebc1984e4323
LarissaAbr2801 <97035851+LarissaAbr2801@users.noreply.github.com>
2022-02-27 21:22:07 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         OnTaskDoneListener listener = System.out::println;
         OnTaskErrorListener listener33 = () -> System.out.println("Error!");
-        Worker worker = new Worker(listener, listener33);
+        var worker = new Worker(listener, listener33);
         worker.start();
     }
 }
